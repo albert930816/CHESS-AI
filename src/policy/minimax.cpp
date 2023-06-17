8e07575d7e9551621a24219043f35ca81293ae4b
albert930816 <albert930816@gmail.com>
2023-06-18 01:52:14 +0800

    Minimax v3

--- src/policy/minimax.cpp
+++ src/policy/minimax.cpp
@@ -4,41 +4,41 @@
 #include "../state/state.hpp"
 #include "./minimax.hpp"
 
-int Minimax::minimax(State* node, int depth, bool maximizingPlayer, Move& bestMove) {
-    if (depth == 0) {
+
+int Minimax::minimax(State* node,int depth, bool maximizingPlayer){
+    if(depth==0){
         return node->evaluate();
     }
-    if (maximizingPlayer) {
+    if(maximizingPlayer){
         int value = -(2e5);
-        for (auto it : node->legal_actions) {
+        for(auto it:node->legal_actions){
             State* next_move = node->next_state(it);
-            int eval = minimax(next_move, depth - 1, false, bestMove);
-            if (eval > value) {
-                value = eval;
-                bestMove = it;
-            }
+            value = std::max(value,minimax(next_move,depth-1,false));
         }
         return value;
     }
-    else {
+    else{
         int value = 2e5;
-        for (auto it : node->legal_actions) {
+        for(auto it:node->legal_actions){
             State* next_move = node->next_state(it);
-            int eval = minimax(next_move, depth - 1, true, bestMove);
-            if (eval < value) {
-                value = eval;
-                bestMove = it;
-            }
+            value = std::min(value,minimax(next_move,depth-1,true));
         }
         return value;
     }
 }
 
-Move Minimax::get_move(State* state, int depth) {
-    if (!state->legal_actions.size())
-        state->get_legal_actions();
-
-    Move bestMove;
-    minimax(state, depth, state->player, bestMove);
-    return bestMove;
+Move Minimax::get_move(State *state, int depth){
+  Move bestMove;
+  int Max=-2e5;
+  if(!state->legal_actions.size())
+    state->get_legal_actions();
+  
+  for(auto it:state->legal_actions){
+    State* next_move = state->next_state(it);
+    if(Max<minimax(next_move,depth,next_move->player)){
+        Max=minimax(next_move,depth,next_move->player);
+        bestMove=it;
+    }
+  }
+  return bestMove;
 }
